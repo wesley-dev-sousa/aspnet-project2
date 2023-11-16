3c9c978aecd1868d63550bc07bbb3be239e2f584
Wesley Sousa <wesleysousa1121@gmail.com>
2023-11-16 16:05:16 -0300

    fix

--- App.Web/wwwroot/js/core/requester.js
+++ App.Web/wwwroot/js/core/requester.js
@@ -141,7 +141,7 @@ function GetApiAddress() {
         if (window.location.host.indexOf('demo') > -1) {
             return "https://demoautenticador.abase.com.br/api/";
         }
-        return "https://localhost:44368/";//Endereço em que está compilando a api
+        return "https://localhost:7142/";//Endereço em que está compilando a api
     }
     return new URL(window.location.href).origin + "/api/";
 }
