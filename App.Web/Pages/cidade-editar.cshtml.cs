effc6ee8abc0de1c4456e31319dc15f12e5dba54
Wesley Sousa <wesleysousa1121@gmail.com>
2023-11-17 09:28:11 -0300

    fix erros || Add Interface

---
+++ App.Web/Pages/cidade-editar.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace App.Web.Pages
+{
+    public class cidade_editarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
