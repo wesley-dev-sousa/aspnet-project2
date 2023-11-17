38a7a228985ce061730d9f34b48eec46683ebc61
Wesley Sousa <wesleysousa1121@gmail.com>
2023-11-17 11:13:37 -0300

    Add Interface

---
+++ App.Web/Pages/Cidade_Editar.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace App.Web.Pages
+{
+    public class Cidade_EditarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
