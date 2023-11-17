ae13b22765608e86add2930f5f830d6ef8af9a53
Wesley Sousa <wesleysousa1121@gmail.com>
2023-11-17 16:21:35 -0300

    Sistema de Filtro Adicionado

--- App.Web/wwwroot/js/pages/cidades-editar.js
+++ App.Web/wwwroot/js/pages/cidades-editar.js
@@ -10,4 +10,29 @@
     }, function (err) {
         alert(err);
     });
+
+}
+$(document).ready(function () {
+    $('#busca').keypress(function (e) {
+        if (e.which === 13) {
+            load();
+        }
+    });
+    load();
+});
+
+function load() {
+    let cidade = $('[name="busca"]').val();
+    CidadeListaCidades(cidade).then(function (data) {
+        $('#table tbody').html('');
+        data.forEach(obj => {
+            $('#table tbody').append('' +
+                '<tr id="obj-' + obj.id + '">' +
+                '<td>' + (obj.id) + '</td>' +
+                '<td>' + (obj.cep || '--') + '</td>' +
+                '<td>' + (obj.nome || '--') + '</td>' +
+                '<td>' + (obj.uf || '--') + '</td>' +
+                '</tr>');
+        });
+    });
 }
